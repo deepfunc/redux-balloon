14319dd1e03f765181a17809a6fff7369bc7dce9
deepfunc <6261625@qq.com>
2020-08-26 21:11:35 +0800

    refactor: actionModules

--- src/actionModules.ts
+++ src/actionModules.ts
@@ -3,14 +3,7 @@ import { ActionDefinitionTuple, ActionsDefinition } from './types/actions';
 import { Model } from './types/model';
 import { StringIndexObject } from './types/utils';
 import { actionDefiner, isActionDefinitionTuple } from './actionDefiner';
-import {
-  assocPath,
-  dissocPath,
-  mapObjIndexed,
-  pathArrayOfNS,
-  isArray,
-  identity
-} from './utils';
+import { assoc, dissoc, identity, forEachObjIndexed } from './utils';
 
 function addActionModule(
   model: Model<any, any, any>,
@@ -21,54 +14,45 @@ function addActionModule(
     return existingModules;
   }
 
-  return assocPath(pathArrayOfNS(namespace), [actions], existingModules);
+  return assoc(namespace, actions, existingModules);
 }
 
 function delActionModule(
   namespace: string,
   existingModules: StringIndexObject
 ): StringIndexObject {
-  return dissocPath(pathArrayOfNS(namespace), existingModules);
+  return dissoc(namespace, existingModules);
 }
 
 function createActions(modules: StringIndexObject): StringIndexObject {
   const actionMap: StringIndexObject = {};
 
-  const create: (
-    modules: StringIndexObject | [ActionsDefinition<any>]
-  ) => StringIndexObject = modules => {
-    if (isArray(modules)) {
-      const [actionDefFunc] = modules;
-      const actionDefMap = actionDefFunc(actionDefiner);
+  const createActionMap = (actionDefFunc: ActionsDefinition<any>): void => {
+    const actionDefMap = actionDefFunc(actionDefiner);
 
-      return mapObjIndexed(
-        (item: string | ActionDefinitionTuple<any, any>, key) => {
-          let action: Function;
-          if (isActionDefinitionTuple(item)) {
-            const type = item[0];
-            const payloadCreator = item[1] != null ? item[1] : identity;
-            const metaCreator = item[2];
-            if (metaCreator == null) {
-              action = createAction(type, payloadCreator);
-            } else {
-              action = createAction(type, payloadCreator, metaCreator);
-            }
-          } else {
-            action = createAction(item);
-          }
+    forEachObjIndexed((item: string | ActionDefinitionTuple<any, any>, key) => {
+      let action: Function;
+      if (isActionDefinitionTuple(item)) {
+        const type = item[0];
+        const payloadCreator = item[1] != null ? item[1] : identity;
+        const metaCreator = item[2];
+        if (metaCreator == null) {
+          action = createAction(type, payloadCreator);
+        } else {
+          action = createAction(type, payloadCreator, metaCreator);
+        }
+      } else {
+        action = createAction(item);
+      }
 
-          actionMap[key] = action;
-          return action;
-        },
-        actionDefMap
-      );
-    }
-
-    return mapObjIndexed(mod => create(mod), modules);
+      actionMap[key] = action;
+    }, actionDefMap);
   };
 
-  const actions = create(modules);
-  return { ...actions, ...actionMap };
+  forEachObjIndexed(actionDefFunc => {
+    createActionMap(actionDefFunc);
+  }, modules);
+  return actionMap;
 }
 
 export { addActionModule, delActionModule, createActions };
