639fbe3430cb6e18eb895d07a62df2ca9114e63e
deepfunc <6261625@qq.com>
2020-10-16 11:42:42 +0800

    refactor: replaced "export *".

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,80 @@ export { REDUCER_ROOT_NAMESPACE, NAMESPACE_SEP } from './constants';
 export { BizStatus } from './constants';
 export { ApiStatus } from './models/api/constants';
 
-// types
-export * from './types/actions';
-export * from './types/reducers';
-export * from './types/selectors';
-export * from './types/sagas';
-export * from './types/model';
-export * from './types/balloon';
-export * from './types/models/apiModel';
+/* export types */
+export type { Biz, BizRunOptions } from './types/balloon';
+
+export type {
+  Action,
+  ActionMeta,
+  ActionCreator,
+  PayloadCreator,
+  MetaCreator,
+  ActionDefinitionTuple,
+  MetaOfApiAction,
+  PayLoadOfAction,
+  MetaOfAction,
+  ApiAction,
+  MetaOfPromiseAction,
+  PromiseAction,
+  DefApiActionFunc,
+  DefPromiseActionFunc,
+  ActionDefiner,
+  ActionCreatorsMapObject,
+  ActionsDefinitionMapObject,
+  ActionsDefinition,
+  ActionsDefinitionReturnType,
+  ActionKey,
+  ActionFuncType,
+  GetActionFunc
+} from './types/actions';
+
+export type { Model } from './types/model';
+
+export type {
+  CreateReducersOptions,
+  EnhanceReducerFunc,
+  ReducersDefinition
+} from './types/reducers';
+
+export type {
+  SagaErrorType,
+  Saga,
+  SagaHelperFuncOptions,
+  SagaHelperFuncName,
+  SagaFunc,
+  SagaFuncTuple,
+  SagasDefinitionMapObject,
+  SimpleSagaFunc,
+  SimpleSagaFuncTuple,
+  SimpleSagasDefinitionMapObject,
+  SimpleSagasDefinitionFunc,
+  ManualSagasDefinitionFunc,
+  SagasDefinition
+} from './types/sagas';
+
+export type {
+  ReselectObject,
+  SelectorsMapObject,
+  SelectorsDefinition,
+  SelectorKey,
+  SelectorFuncType,
+  GetSelectorFunc
+} from './types/selectors';
+
+export type {
+  StringIndexObject,
+  NonNullableProperties,
+  NonNullableAndRequiredProperties
+} from './types/utils';
+
+export type {
+  ApiModelOptions,
+  ApiStatusInfo,
+  ApiModelState,
+  InitApiStatusAction,
+  ApiModelActions,
+  ApiModelSelectors
+} from './types/models/apiModel';
 
 export default balloon;
